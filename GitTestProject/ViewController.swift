24d54fcbbfefd400cc9d96da10b64623cf7e6fcc
lvntoztrkswift <leventozturk1905@yandex.com>
2021-04-11 19:40:22 +0300

    deneme

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by LEVENT ÖZTÜRK on 11.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    
+        print("git")
+    }
+
+    
+
+}
+
